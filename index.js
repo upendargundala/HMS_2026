40478349903c3161e0df2d8beb4d68fe14f81cc9
GUNDALA UPENDAR <142022758+upendargundala@users.noreply.github.com>
2025-08-07 11:01:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Middleware
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+app.use('/api/auth', authRoutes);   
+// Error Handling Middleware
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
